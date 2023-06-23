ea59378da2910fb9f03dc20d128919834db84144
carlosICQ <133284401+carlosICQ@users.noreply.github.com>
2023-06-23 16:35:56 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var cuadrito
+var cuadrito2
+var cuadrito3
+var cuadrito4
+var cuadrito5
+var cuadrito6
+var cuadrito7
+var cuadrito8
+var cuadrito9
+var cuadrito10
+var cuadrito11
+var cuadrito12
+var cuadrito13
+var cuadrito14
+var cuadrito15
+var cuadrito16
+var cuadrito17
+
+function setup() {
+  createCanvas(1000, 400);
+  cuadrito = createSprite(10,10,20,100)
+  cuadrito2 = createSprite(10,10,100,20)
+  cuadrito3 = createSprite(10,50,100,20)
+  cuadrito4 = createSprite(150,10,70,20)
+  cuadrito5 = createSprite(120,10,20,90)
+  cuadrito6 = createSprite(150,50,70,20)
+  cuadrito7 = createSprite(180,30,20,90)
+  cuadrito8 = createSprite(260,30,20,70)
+  cuadrito9 = createSprite(280,10,70,20)
+  cuadrito10 = createSprite(400,30,20,90)
+  cuadrito11 = createSprite(500,30,20,90)
+  cuadrito12 = createSprite(520,60,90,20)
+  cuadrito13 = createSprite(560,30,20,90)
+  cuadrito14 = createSprite(520,10,90,20)
+  cuadrito15 = createSprite(690,60,90,20)
+  cuadrito16 = createSprite(740,10,20,90)
+  cuadrito17 = createSprite(760,10,90,20)
+
+
+  //Crea los sprites necesarios para formar tu nombre
+
+}
+
+
+function draw() {
+  background("black");
+  drawSprites();
+  
+}
